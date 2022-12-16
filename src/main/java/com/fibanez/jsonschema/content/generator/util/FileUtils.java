524bbb465293b984d774a7923887705a8be51185
Fernando Ibanez <fernando.ibanez@bjss.com>
2022-12-16 13:30:10 +1100

    Fix form reading content-media files from resources

--- src/main/java/com/fibanez/jsonschema/content/generator/util/FileUtils.java
+++ src/main/java/com/fibanez/jsonschema/content/generator/util/FileUtils.java
@@ -4,18 +4,15 @@ import com.fibanez.jsonschema.content.generator.exception.GeneratorException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.InputStream;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public final class FileUtils {
 
     public static byte[] getImageResource(String resourceName) {
         try {
-            URL url = Thread.currentThread().getContextClassLoader().getResource(resourceName);
-            Path path = Path.of(url.toURI());
-            return Files.readAllBytes(path);
+            InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourceName);
+            return inputStream.readAllBytes();
         } catch (Exception ex) {
             throw new GeneratorException(ex, "Resource not found %s", resourceName);
         }
